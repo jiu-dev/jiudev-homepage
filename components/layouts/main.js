cda272777cc5eea10a164fcb97d084d5a7923f20
FAREEA Matahi <fareeamatahi@FAREEAs-MBP.lan>
2023-09-25 15:53:16 +0200

    fix copyright

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -17,7 +17,7 @@ const Main = ({ children, router }) => {
 
       <div className="flex items-center justify-center bg-firstplan w-full h-screen sm:p-14 md:p-20 xl:p-40 ">
         <span className="absolute hidden whitespace-nowrap sm:block sm:top-7 md:top-10 xl:top-20  left-1/2 -translate-y-1/2 -translate-x-1/2">
-          <MessageBubble message="Bonjour ! Bienvenu sur mon Portfolio :)" />
+          <MessageBubble message="Bonjour ! Je m'appelle Matahi, bienvenue sur mon Portfolio :)" />
         </span>
         <div className="h-full w-full flex flex-col text-white bg-white/20 backdrop-blur-lg overflow-hidden border-4 border-white rounded-3xl">
           <div className="px-8 py-4 sm:h-1/6 bg-indigo-300/40 sm:px-8">
@@ -30,7 +30,7 @@ const Main = ({ children, router }) => {
             <Footer />
           </div>
         </div>
-        <div className="hidden bottom-6 sm:block sm:absolute">
+        <div className="hidden bottom-6 whitespace-nowrap sm:block sm:absolute sm:bottom-7 md:bottom-10 xl:bottom-20 left-1/2 translate-y-1/2 -translate-x-1/2">
           <Copyright />
         </div>
       </div>
